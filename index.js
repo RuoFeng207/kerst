4615f5c3b6c47de48ed7bc25804d16183213a810
RuoFeng207 <99081157@mydavinci.nl>
2024-12-16 21:27:37 +0100

    update

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ document.getElementById("bevestig").onclick=function(){
     gebruikers_naam=document.getElementById("naam").value;
     document.getElementById("welkom").textContent=`Merry christmas, ${gebruikers_naam}!!!`
 }
+
+// achtergrond muziek
+window.addEventListener("click", ()=>{
+    document.getElementById("song").play();
+})
