20fb29b79f5719e4267de9a96c8681fa0d89ce17
RuoFeng207 <99081157@mydavinci.nl>
2024-12-21 17:38:34 +0100

    update

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let gebruikers_naam;
+let legeString = " "; // Dit is een spatie
 document.getElementById("bevestig").onclick=function(){
     gebruikers_naam=document.getElementById("naam").value;
     document.getElementById("welkom").textContent=`Merry christmas, ${gebruikers_naam}!!!`
     document.getElementById("bericht").textContent=`Hallo, ${gebruikers_naam}
-
+${legeString}
 ik wens jou een heel fijne 
 kerst.
-
+${legeString}
 En een gelukkig nieuwjaar.
-
+${legeString}
 Ik hoop dat al je wensen 
 mogen uitkomen. 
 kerstige groeten,
-
+${legeString}
 Ruo Feng   `
 }
 
