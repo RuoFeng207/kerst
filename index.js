72b44672c2203450101dd9632463e3f5baa5485f
RuoFeng207 <99081157@mydavinci.nl>
2024-12-22 15:25:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ let gebruikers_naam;
 document.getElementById("bevestig").onclick=function(){
     gebruikers_naam=document.getElementById("naam").value;
     document.getElementById("welkom").textContent=`Merry christmas, ${gebruikers_naam}!!!`
-    document.getElementById("bericht").textContent=`Hallo
-    mijn test`
+    document.getElementById("bericht").innerHTML = `Hallo, ${gebruikers_naam}<br><br>IK wens jou een heel fijne kerst.<br>
+    En een gelukkig nieuwjaar.<br><br>
+    Ik hoop dat al je wensen mogen uitkomen.<br><br>
+    Kerstige groet, <br><br>
+    Ruo Feng `;
 }
 
 // achtergrond muziek
